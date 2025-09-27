17b9f7ab2523c0eb7086accdaf9e67b952160bcd
akili <sedov.anton1986@gmail.com>
2025-09-27 10:35:06 +0300

    Используем cmd, можем войти в приложение и выйти

--- main.py
+++ main.py
@@ -1,6 +1,32 @@
-def main():
-    print("Hello from in-memory-db!")
+import cmd
+from typing import Any
+
+
+class TinyDB(cmd.Cmd):
+    """Очень простая консольная in-memory БД."""
+
+    intro = (
+        "Добро пожаловать в TinyDB! Введите help или ? для списка команд.\n"
+    )
+    prompt = "tiny_db> "
+
+    def __init__(self) -> None:
+        super().__init__()
+        self.storage = {}
+
+    def cmdloop(self, intro: Any | None = None) -> None:
+        """REPL для приложения."""
+        try:
+            super().cmdloop(intro=intro)
+        except KeyboardInterrupt:
+            # Выход по ctrl+c
+            print("\nПока!")
+
+    def do_EOF(self, _: str) -> bool:  # noqa: N802
+        """Выйти по ctrl+d."""
+        print("\nПока!")
+        return True
 
 
 if __name__ == "__main__":
-    main()
+    TinyDB().cmdloop()
